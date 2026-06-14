df586f4f8e19210bed17dc55cb33210613cc7302
melody <admin@melthecat.dev>
2026-06-14 13:48:37 +0200

    add WAL support and bind-mount separate data dir by default

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# OOYE Docker container
+# OOYE OCI image
 
-This repo contains buildscripts and instructions to setup and run ooye inside a docker conatiner.
+This repo contains buildscripts and instructions to setup and run ooye inside an oci-compatible runtime like Podman or Docker.
 
 ## Setup
 - Copy compose.yaml from repo
-- Uncomment the command line in `compose.yaml`
-- Make sure `ooye.db` and `registration.yaml` exist by creating them with `touch ooye.db registration.yaml`
+- Make sure to create the data directory (by default `./ooye-data`) and is owned by UID:GID 1000:1000
 - Run `docker compose up -d` to start the container in the background
-- You now got 30 mins time to setup the container
-- Run `docker compose exec ooye npm run setup` to start the interactive setup process
-- After setup is complete, remove or comment out the command line inside the `compose.yaml` file again and restarting the container by running `docker compose down && docker compose up -d`
+- Follow the prompts
+
+## Repos
+- Main repo: [git.shork.ch/oci-images/out-of-your-element](https://git.shork.ch/oci-images/out-of-your-element)
+- Mirrors:
+    - Codeberg: [melthecat/ooye-oci](https://codeberg.org/melthecat/ooye-oci)
+    - Github: [melthecat/ooye-docker](https://github.com/melthecat/ooye-docker)
 
 ## Other info
 - [ooye project source](https://gitdab.com/cadence/out-of-your-element)
---
+++ entrypoint.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env sh
+set -e
+
+touch "$OOYE_DATADIR/ooye.db"
+
+if [ "$OOYE_WAL" = "true" ] && [ "$OOYE_DATADIR" = "/usr/src/app" ]; then
+    echo "WAL mode cannot be enabled when OOYE_DATADIR isn't set."
+    echo "The directory set should also be bind-mounted."
+    exit 1
+fi
+
+if [ "$OOYE_DATADIR" != "/usr/src/app" ]; then
+    ln -s "$OOYE_DATADIR/ooye.db" "/usr/src/app/ooye.db"
+    ln -s "$OOYE_DATADIR/registration.yaml" "/usr/src/app/registration.yaml"
+    echo "Database symlink to $OOYE_DATADIR/ooye.db created!"
+else 
+    echo "Not setting OOYE_DATADIR is deprecated and might be removed in the future."
+    echo "Please set it to a writeable directory and bind-mount it to persist your data."
+    echo "Make sure to move your existing data to the new directory."
+fi
+
+if [ ! -s "$OOYE_DATADIR/ooye.db" ]; then
+    echo "Database file is empty, sleeping for 30 mins."
+    echo "Run setup by executing \`docker compose exec ooye npm run setup\`"
+    echo "After setup, run \`docker compose down && docker compose up -d\`"
+    sleep 1800
+fi
+
+if [ "$OOYE_WAL" = "true" ]; then
+    node << EOF
+const sqlite = require("better-sqlite3")
+const db = new sqlite("/usr/src/app/ooye.db", {fileMustExist: true})
+db.pragma("journal_mode = wal")
+db.close()
+EOF
+    ln -s "$OOYE_DATADIR/ooye.db-wal" "/usr/src/app/ooye.db-wal"
+    ln -s "$OOYE_DATADIR/ooye.db-shm" "/usr/src/app/ooye.db-shm"
+    echo "WAL mode enabled!"
+else 
+    node << EOF
+const sqlite = require("better-sqlite3")
+const db = new sqlite("/usr/src/app/ooye.db", {fileMustExist: true})
+db.pragma("journal_mode = delete")
+db.close()
+EOF
+    rm -f "/usr/src/app/ooye.db-wal" "/usr/src/app/ooye.db-shm" "$OOYE_DATADIR/ooye.db-wal" "$OOYE_DATADIR/ooye.db-shm"
+    echo "WAL mode disabled!"
+fi
+
+npm run start
